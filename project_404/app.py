b7399b2d5db0dcde5d2a92ac66c412c38ea6dcf8
gregthemitch <gmitchelljr@uchicago.edu>
2024-03-03 09:22:26 -0600

    Updated project files including app.py

--- project_404/app.py
+++ project_404/app.py
@@ -5,38 +5,51 @@ def data_collection():
     pass
 
 
-def start_chatbot():
-    function_calling_bot = function_calling_agent()
-    response_bot = response_agent()
+def start_chatbot(ngrok_tunnel_key):
+    """
+    This function uses a 3-stage process whereby a user can input natural
+    language that is converted into python function parameters, and 
+    then is summarized back to the user.
+    
+    """
+    function_calling_bot = function_calling_agent(ngrok_tunnel_key)
+    response_bot = response_agent(ngrok_tunnel_key)
+
     while True:
-        prompt = input(">> ")
+        prompt = input(">>> ")
 
-
-        
-        if prompt == "quit":
+        if prompt in ["quit", "quit()"]:
             break
+        
+        answers = function_calling_bot.call_functions(prompt)
+        response_bot.respons_with_answers(answers)
 
 
 def run():
     if len(sys.argv) != 2:
         print(f"Usage: python3 {sys.argv[0]} <data_collection> OR <climate_bot>")
-        sys.exit(1)
+        sys.exit()
 
     if sys.argv[1] == "data_collection":
         print("Start data collection process? (y/n)")
-        response = input(">> ")
+        response = input(">>> ")
 
         if response in ["y", "yes", "Y", "Yes"]:
            data_collection()
         else:
-            sys.exit(1)
+            sys.exit()
 
     if sys.argv[1] == "climate_bot":
         print("Start climate chatbot? (y/n)")
-        response = input(">> ")
+        response = input(">>> ")
 
         if response in ["y", "yes", "Y", "Yes"]:
-           start_chatbot()
+           print("Please input Ngrok tunnel key")
+           print("The key should mostly resemble <https://####-##-###-###-##.ngrok-free.app>")
+           ngrok_tunnel_key = input(">>> ")
+
+           start_chatbot(ngrok_tunnel_key)
+
         else:
-            sys.exit(1)
+            sys.exit()
         
