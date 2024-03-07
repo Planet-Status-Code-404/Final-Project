f73996bee4c648ac05b089df7d0721d6e44be4a1
gregthemitch <gmitchelljr@uchicago.edu>
2024-03-07 04:16:40 -0600

    Updated

--- project_404/data_collection/sql_database.py
+++ project_404/data_collection/sql_database.py
@@ -71,7 +71,3 @@ def insert_tables_to_database():
     print("Database created successfully and Tables added")
     connection.commit()
     connection.close()
-
-
-# Executing the function to add data in the directory to the database
-insert_tables_to_database()
